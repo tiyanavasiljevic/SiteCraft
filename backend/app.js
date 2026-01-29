ccbae6f09cd04a2bf1eb17e1e8b91fb087c5ac82
Tijana Vasiljevic <ms.tijana.vasiljevic@gmail.com>
2026-01-29 11:02:43 +0100

    deleting static dist path

--- backend/app.js
+++ backend/app.js
@@ -20,10 +20,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 // 🚀 Serviranje frontenda iz dist foldera
-app.use(express.static(path.join(__dirname, 'dist')));
+//app.use(express.static(path.join(__dirname, 'dist')));
 
 // 🎯 Ako ruta nije API, vrati index.html (SPA fallback)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+//app.get('*', (req, res) => {
+  //res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+//});
+
+app.get('/', (req, res) => {res.send("server is free")});
 export default app;
